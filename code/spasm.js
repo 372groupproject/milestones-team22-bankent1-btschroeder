110104c58553a6f31e5a0d2aaddf90a0bb0de189
bankent1 <bankent1@email.arizona.edu>
2020-04-12 13:02:24 -0700

    Added runtime error

--- code/spasm.js
+++ code/spasm.js
@@ -19,7 +19,7 @@ Notes for later:
 */
 
 //load these guys from the server
-const filepaths = ["./p2_if.wasm", "./p2_br_table.wasm", "./p2_loop.wasm", "./p3_i32.wasm", "./p3_i64.wasm", "./p3_f32.wasm"]
+const filepaths = ["./p2_if.wasm", "./p2_br_table.wasm", "./p2_loop.wasm", "./p3_i32.wasm", "./p3_i64.wasm", "./p3_f32.wasm", "./p3_eruntime.wasm"]
 
 //wasm functions we can call
 let funcs = {}
